cbea804cab83998eb52bbe418424d06dc8fadbc4
aymenelidrissi4 <aymenelidrissi4@gmail.com>
2024-03-13 16:48:12 +0000

    new commit

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+        var test = SecurityContextHolder.getContext().getAuthentication();
     }
 
     @Bean
